04198796fe074e338f5510b1faa5fe7cef727cd7
Ruud Andriessen <r.andriessen@ilysian.com>
2016-10-10 10:11:40 +0200

    Unity v1.0

---
+++ Assets/source/CreateBlock.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBlock : MonoBehaviour {
+	public float x, z;
+
+	// Use this for initialization
+	void Start () {
+		GameObject obj = this.gameObject;
+
+		float width = 1.0f;
+		float height = 1.0f;
+		float depth = 1.0f;
+
+		x = x / obj.transform.lossyScale.x;
+		z = z / obj.transform.lossyScale.z;
+
+		// Left side
+		createCube (obj.transform, 
+			new Vector3(-width / 2, 0, 0), 
+			new Vector3(0.01f, height, depth));
+		// Right side
+		createCube (obj.transform, 
+			new Vector3(width / 2, 0, 0), 
+			new Vector3(0.01f, height, depth));
+
+
+		// Back side
+		createCube (obj.transform, 
+			new Vector3(0, 0, depth / 2), 
+			new Vector3(width, height, 0.01f));
+		// Front side
+		createCube (obj.transform, 
+			new Vector3(0, 0, -depth / 2), 
+			new Vector3(width, height, 0.01f));
+
+		// Bottom side
+		createCube (obj.transform, 
+			new Vector3(0, - height / 2, 0), 
+			new Vector3(width, 0.01f, depth));
+
+
+		createTop (obj.transform, width, height, depth);
+	}
+
+	GameObject createCube(Transform parent, Vector3 position, Vector3 scale) {
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.parent = parent;
+		cube.transform.localPosition = position;
+		cube.transform.localScale = scale;
+		return cube;
+	}
+
+	GameObject createTop (Transform parent, float width, float height, float depth) {
+		GameObject top = new GameObject ();
+		top.transform.parent = parent;
+		top.transform.localPosition = new Vector3(0, height / 2, 0);
+		top.transform.localScale = new Vector3(1, 1, 1);
+
+		float xLeftRight = width / 2 - x / 2;
+
+		// Left of hole
+		createCube (top.transform, 
+			new Vector3(- width / 2 + xLeftRight / 2, 0, 0),
+			new Vector3(xLeftRight, 0.01f, depth));
+
+		// Right of hole
+		createCube (top.transform, 
+			new Vector3(width / 2 - xLeftRight / 2, 0, 0),
+			new Vector3(xLeftRight, 0.01f, depth));
+
+		float zTopBottom = depth / 2 - z / 2;
+
+		// Top of hole
+		createCube (top.transform, 
+			new Vector3(0, 0, - depth / 2 + zTopBottom / 2),
+			new Vector3(x, 0.01f, zTopBottom));
+
+		// Bottom of hole
+		createCube (top.transform, 
+			new Vector3(0, 0, + depth / 2 - zTopBottom / 2),
+			new Vector3(x, 0.01f, zTopBottom));
+
+		// Right of hole
+		return top;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/source/Run.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Run : MonoBehaviour {
+	public GameObject dock;
+
+	// Use this for initialization
+	void Start () {
+		// Data variables
+		int amount_experiments = 0;
+
+		// Experiment static values
+		float hole_min = 0.1f, hole_max = maxSize (dock);
+		float stepSize = 0.05f;
+		int batch_size = 15;
+
+
+		// Experiment values
+		float I, deltaI, k = 0.2f;
+		float correct_percent_batch = 0;
+
+		int iteration_count = 5;
+		int batch_count = 3 * iteration_count;
+		ArrayList<bool> R = new ArrayList<bool> ();
+
+		while (amount_experiments < batch_count && correct_percent_batch - 0.5f >= 0.1f) {
+			// Run one iteration
+			for (int i = 0; i < iteration_count; i++) {
+				I = Random.Range (hole_min, hole_max);
+				deltaI = k * I;
+				createBlock (deltaI);
+				R.Add(waitForExperiment(I, deltaI));
+//				batch.Add (result);
+				log ();
+//				if (batch.Count > batch_count)
+//					batch.RemoveFirst ();
+				amount_experiments++;
+			}
+
+//			correct_percent = calculatePercentage (batch);
+			if (correct_percent > 0.6f) {
+				deltaI += stepSize;
+			} else {
+				deltaI -= stepSize;
+			}
+		}
+
+	}
+
+	bool waitForExperiment() {
+		return false;
+	}
+
+//	float calculatePercentage(ArrayList<bool> batch) {
+//		int sum = 0;
+//		foreach (bool b in batch) {
+//			if (b)
+//				sum++;
+//		}
+//		return (float) sum / (float) batch.Count;
+//	}
+
+	void createBlock(float delta) {
+		CreateBlock script = dock.AddComponent<CreateBlock>();
+		script.x = delta;
+		script.z = delta;
+	}
+
+	void log() {
+	}
+
+	float maxSize(GameObject gobj) {
+		float x = gobj.transform.lossyScale.x;
+		float z = gobj.transform.lossyScale.z;
+		return x >= z ? x : z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
