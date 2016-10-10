fb39fce864b7376983b0d6ff5bb97ba66e74125d
Xander Houtman <g.a.houtman@student.tue.nl>
2016-10-10 14:30:23 +0200

    Attempt to add camera

--- Assets/source/MouseOrbitImproved.cs
+++ Assets/source/MouseOrbitImproved.cs
@@ -5,6 +5,8 @@ using System.Collections;
 public class MouseOrbitImproved : MonoBehaviour {
 
 	public Transform target;
+	public Transform altTarget;
+
 	public float distance = 5.0f;
 	public float xSpeed = 120.0f;
 	public float ySpeed = 120.0f;
@@ -15,7 +17,7 @@ public class MouseOrbitImproved : MonoBehaviour {
 	public float distanceMin = .5f;
 	public float distanceMax = 15f;
 
-	private Rigidbody rigidbody;
+	//private Rigidbody rigidbody;
 
 	float x = 0.0f;
 	float y = 0.0f;
@@ -27,32 +29,43 @@ public class MouseOrbitImproved : MonoBehaviour {
 		x = angles.y;
 		y = angles.x;
 
-		rigidbody = GetComponent<Rigidbody>();
+		//rigidbody = GetComponent<Rigidbody>();
 
 		// Make the rigid body not change rotation
-		if (rigidbody != null)
+		/*if (rigidbody != null)
 		{
 			rigidbody.freezeRotation = true;
-		}
+		}*/
 	}
 
 	void LateUpdate () 
 	{
+		Debug.Log ("Camera script runs");
 		if (Input.GetMouseButtonDown(0))
 		{
 			Cursor.lockState = CursorLockMode.Locked;
-			Cursor.visible = false;
+			//Cursor.visible = false;
 		}
 		if (Input.GetMouseButtonUp(0))
 		{
 			Cursor.lockState = CursorLockMode.None;
-			Cursor.visible = true;
+			//Cursor.visible = true;
 		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.RightArrow)) {
+			if (altTarget != null) {
+				Transform temp = target;
+				target = altTarget;
+				altTarget = temp;
+			}
+		}
+
 		if (target) 
 		{
+			float distSpeedModifier = 1.0f / Mathf.Sqrt (distance);
 			if (Input.GetMouseButton (0)) {
-				x += Input.GetAxis ("Mouse X") * xSpeed * distance * 0.02f;
-				y -= Input.GetAxis ("Mouse Y") * ySpeed * 0.02f;
+				x += Input.GetAxis ("Mouse X") * xSpeed * distance * 0.02f * distSpeedModifier;
+				y -= Input.GetAxis ("Mouse Y") * ySpeed * 0.02f * distSpeedModifier;
 			}
 			y = ClampAngle(y, yMinLimit, yMaxLimit);
 
@@ -60,11 +73,11 @@ public class MouseOrbitImproved : MonoBehaviour {
 
 			distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel")*5, distanceMin, distanceMax);
 
-			RaycastHit hit;
+			/*RaycastHit hit;
 			if (Physics.Linecast (target.position, transform.position, out hit)) 
 			{
 				distance -=  hit.distance;
-			}
+			}*/
 			Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
 			Vector3 position = rotation * negDistance + target.position;
 
