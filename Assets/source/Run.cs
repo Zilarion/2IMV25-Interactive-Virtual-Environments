9a605ffe9b00d2a9b76d2d50297d9473a1064aff
Ruud Andriessen <r.andriessen@ilysian.com>
2016-10-10 11:00:51 +0200

    Updated materials and scene

--- Assets/source/Run.cs
+++ Assets/source/Run.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 public class Run : MonoBehaviour {
 	public GameObject dock;
 
+	bool runningExperiment = false;
+
 	// Use this for initialization
 	void Start () {
 		// Data variables
@@ -17,8 +19,8 @@ public class Run : MonoBehaviour {
 		int step_count = 5;
 
 		// Experiment values
-		float I, deltaI, k = 0.2f;
-		float correct_percent_batch;
+		float I = 0.0f, deltaI = 0.0f, k = 0.2f;
+		float correct_percent_batch = 0.0f;;
 		float correct_percent;
 
 		List<bool> R = new List<bool> ();
@@ -47,7 +49,7 @@ public class Run : MonoBehaviour {
 		}
 	}
 
-	bool waitForExperiment() {
+	bool waitForExperiment(float I, float deltaI) {
 		return false;
 	}
 
@@ -79,9 +81,18 @@ public class Run : MonoBehaviour {
 		float z = gobj.transform.lossyScale.z;
 		return x >= z ? x : z;
 	}
-	
+
+	void experimentComplete(bool choice) {
+		
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (runningExperiment) {
+			if (Input.GetKeyDown ("up"))
+				experimentComplete (true);
+			if (Input.GetKeyDown ("down"))
+				experimentComplete (false);
+		}
 	}
 }
